aae391b97692a102ff7ca890091a29f3e8ff6e60
churabou <ginga7888@gmail.com>
2019-03-21 12:19:52 +0900

    fuck router

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div>
     <app-header></app-header>
+    <p>
+      <router-link to="/foo">About</router-link>
+    </p>
+    
     <chart-section/>
+    <login-form/>
   </div>
 </template>
 
 <script>
 import AppHeader from './components/app-header';
 import ChartSection from './components/chart-section';
+import LoginForm from './components/login-form';
 
 const data = {
   components: {
     AppHeader,
-    ChartSection
+    ChartSection,
+    LoginForm
   },
 }
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
+import VueRouter from 'vue-router'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+
+import Login from './Login.vue'
+
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
+Vue.use(VueRouter)
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+const router = new VueRouter({
+  routes // `routes: routes` の短縮表記
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
+
