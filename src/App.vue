cd24035c015e8060d06683f6a5085a3d6b4b4c99
churabou <ginga7888@gmail.com>
2019-03-21 10:24:38 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <myheader></myheader>
+    <p v-if="msg.length > 0">
+      {{msg}}
+    </p>
+    <p v-else>
+      no text
+    </p>
+    <input type="text" v-model="msg">
+    <button @click="clear()">clear</button>
+    <MyComponent/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import myheader from './components/myheader'
+import MyComponent from './components/MyComponent.vue'
 
-export default {
-  name: 'app',
+const mouting = {
   components: {
-    HelloWorld
+    myheader,
+    MyComponent
+  },
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
+  },
+  methods: {
+    clear () {
+      this.msg = ''
+    }
+  },
+  created () {
+    fetch('http://www.geonames.org/postalCodeLookupJSON?postalcode=10504&country=US')
+    .then( response => {
+      return response.json()
+    })
+    .then( json => {
+
+      this.msg = json.postalcodes[0].adminName1
+    })
+    .catch( () => {
+
+    });
   }
 }
-</script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+export default mouting
+</script>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="wrapper">
+    <div class="item-container">
+      <input class="chart-item-title" type="text" v-model="itemTitle">
+      <textarea class="chart-item-textarea"> </textarea>
+    </div>
+    <div class="item-container">
+      <input class="chart-item-title" type="text" name="" value="">
+      <textarea class="chart-item-textarea"> </textarea>
+    </div>
+    <div  class="item-container">
+      <input class="chart-item-title" type="text" name="" value="">
+      <textarea class="chart-item-textarea"> </textarea>
+    </div>
+    <div  class="item-container">
+      <input class="chart-item-title" type="text" name="" value="">
+      <textarea class="chart-item-textarea"> </textarea>
+    </div>
+    <li v-for="item in items">
+      {{ item.message }}
+    </li>
+    <p>{{itemTitle}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  props: {
+    title: String
+  },
+
+  data() {
+    return {
+      items: [
+        { message: 'Foo' },
+        { message: 'Bar' }
+      ],
+      itemTitle: "",
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.wrapper {
+  width: 900px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+
+.item-container {
+  margin-top: 5px;
+  text-align: center;
+}
+
+.chart-item-title {
+  width: 90%;
+  height: 20px;
+  border: 2px #77ddff solid;
+  background-color: white;
+  display: inline-block;
+  margin-bottom: 5px;
+}
+
+.chart-item-textarea {
+  width: 90%;
+  height: 150px;
+  border: 2px #77ddff solid;
+  background-color: white;
+    display: inline-block;
+}
+</style>
